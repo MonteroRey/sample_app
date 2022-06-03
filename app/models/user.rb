775669f109bd687d521ca21df8307d851acce33e
John Doe <johndoe@example.com>
2022-06-03 18:47:26 +0800

    commit 7: generate user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, presence: true
+    ######################## validation for email ###############################
+    #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.(com|net|org)+\z/i
+    validates :email, presence: true ,length: {maximum: 255} , format:{with: VALID_EMAIL_REGEX } ,
+    uniqueness: true
+end
